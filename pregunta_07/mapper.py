9fa1b658dc2816bf8225059fb4617ccd21576c1c
Felipe Giraldo <fgiraldo@summa-sci.com>
2022-06-20 18:02:45 -0500

    add question 07 solution

--- pregunta_07/mapper.py
+++ pregunta_07/mapper.py
@@ -1,3 +1,22 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+#! /usr/bin/ python3
+
+#
+# Esta es la funcion que mapea la entrada a parejas (clave, valor)
+#
+import sys
+if __name__ == "__main__":
+
+    #
+    # itera sobre cada linea de codigo recibida
+    # a traves del flujo de entrada
+    #
+    for line in sys.stdin:
+
+        col_a = line.split()[0]
+        col_b = line.split()[1]
+        col_c = line.split()[2].zfill(3)
+
+        sys.stdout.write("{}\t{}\t{}\n".format(col_a,col_c,col_b))
--- pregunta_07/reducer.py
+++ pregunta_07/reducer.py
@@ -1,3 +1,27 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+#! /usr/bin python3
+
+import sys
+
+#
+# Esta funcion reduce los elementos que tienen la misma clave
+#
+if __name__ == '__main__':   
+
+    #
+    # cada linea de texto recibida es una entrada clave \tabulador valor
+    #
+    for line in sys.stdin:
+
+        key, val1, val2 = line.split("\t")
+
+        if val1 == '000':
+            val1 = '0'
+        else:    
+            val1 = val1.lstrip('0')
+            
+        val2 = val2.strip()
+
+        sys.stdout.write("{}\t{}\t{}\n".format(key, val2, val1))
