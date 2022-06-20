5de5c9be7925882dbfa26172285a87780fb50044
Felipe Giraldo <fgiraldo@summa-sci.com>
2022-06-20 07:16:36 -0500

    add question 05 solution

--- pregunta_05/mapper.py
+++ pregunta_05/mapper.py
@@ -1,3 +1,21 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+#! /usr/bin/ python3
+
+#
+# Esta es la funcion que mapea la entrada a parejas (clave, valor)
+#
+import sys
+if __name__ == "__main__":
+
+    #
+    # itera sobre cada linea de codigo recibida
+    # a traves del flujo de entrada
+    #
+    for line in sys.stdin:
+
+        col_b = line.split()[1]
+        col_b = col_b.split("-")[1]
+
+        sys.stdout.write("{}\t1\n".format(col_b))
