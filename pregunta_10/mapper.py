c6d8f53f4275063766b0e3691bdb59ff37a8dfa6
Felipe Giraldo <fgiraldo@summa-sci.com>
2022-06-20 21:06:09 -0500

    add question 10 solution

--- pregunta_10/mapper.py
+++ pregunta_10/mapper.py
@@ -1,3 +1,30 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+#! /usr/bin/ python3
+
+#
+# Esta es la funcion que mapea la entrada a parejas (clave, valor)
+#
+import sys
+if __name__ == "__main__":
+
+    #
+    # itera sobre cada linea de codigo recibida
+    # a traves del flujo de entrada
+    #
+    for line in sys.stdin:
+
+        #
+        # genera las tuplas palabra \tabulador 1
+        # ya que es un conteo de palabras
+        #
+        col_a = line.split()[0].zfill(2)
+        letters = line.split()[1]
+
+        for letter in letters.split(","):
+
+            #
+            # escribe al flujo estandar de salida
+            #
+            sys.stdout.write("{}\t{}\n".format(letter, col_a))
