b57d31389a280730a59d3856f0f7010bc629d330
Felipe Giraldo <fgiraldo@summa-sci.com>
2022-06-24 17:00:30 -0500

    add question 04 solution

--- pregunta_04/mapper.py
+++ pregunta_04/mapper.py
@@ -1,3 +1,16 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+#! /usr/bin/ python3
+
+#
+# Esta es la funcion que mapea la entrada a parejas (clave, valor)
+#
+import sys 
+ 
+if __name__ == "__main__": 
+    for line in sys.stdin: 
+        # se separan las columnas separadas 
+        lista = line.split(" ") 
+        # se genera la salida del dato llamando la columna objetivo 
+        sys.stdout.write("{},1\n".format(lista[0]))
